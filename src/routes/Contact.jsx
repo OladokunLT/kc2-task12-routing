baa0e89edca464f3aee75baaa62b8c8f4064e5c2
Oladokun Lukman Tunde <lukmanoladokun86@gmail.com>
2022-05-08 19:13:55 +0100

    Contact modified

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,15 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function Contact() {
-  return (
-    <main>
-        <>
-          <h3>Contact</h3>
-          <p>Email: lukmanoladokun86@gmail.com</p>
-          <p>website: https://lukupay.com.ng</p>
-        </>
-    </main>
-  )
+export class Contact extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pageName: "Contact",
+      email: "lukmanoladokun86@gmail.com",
+      location: "Ibadan Nigeria"
+    }
+
+  }
+
+  render() {
+    return (
+      <>
+        <h3> {this.state.pageName} </h3>
+        <p>Email: {this.state.email} </p>
+        <p>Address: {this.state.location} </p>
+      </>
+    )
+  }
 }
 
 export default Contact
+
+
+
+
+
+
+
+// import React from 'react'
+
+// function Contact() {
+//   return (
+//     <main>
+//         <>
+//           <h3>Contact</h3>
+//           <p>Email: lukmanoladokun86@gmail.com</p>
+//           <p>website: https://lukupay.com.ng</p>
+//         </>
+//     </main>
+//   )
+// }
+
+// export default Contact
